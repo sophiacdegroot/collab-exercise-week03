f9aa4405223f621767f4f99c4c7b0b900188634c
Sophia de Groot <sophia.degroot@gmail.com>
2026-05-04 13:32:05 +0200

    Fix GDP per capita calculation order

--- generate_data.py
+++ generate_data.py
@@ -8,5 +8,5 @@ data = pd.DataFrame(
         "year": 2023,
     }
 )
-data.to_csv("gdp_data.csv", index=False)
 data["gdp_per_capita_usd"] = data["gdp_bn_usd"] / data["population_mn"] * 1000
+data.to_csv("gdp_data.csv", index=False)
